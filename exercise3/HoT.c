fb7f43f68a0c3c93d5e3c860fa63dcb1575d156b
unknown <s1302007@u-aizu.ac.jp>
2024-07-04 22:40:22 +0900

    User name added

--- exercise3/HoT.c
+++ exercise3/HoT.c
@@ -3,14 +3,23 @@
 #include <time.h>
 
 int main() {
+    
     srand(time(0));
 
+    char name[50];
     int heads = 0, tails = 0;
+
+    // Ask for the user's name
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+    // Greet the user
+    printf("Hello, %s! Let's start the coin tossing game.\n\n", name);
 
     printf("Tossing a coin...\n");
 
     for (int round = 1; round <= 3; round++) {
-        int toss = rand() % 2; 
+        int toss = rand() % 2;
         if (toss == 0) {
             printf("Round %d: Heads\n", round);
             heads++;
